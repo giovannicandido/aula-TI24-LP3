ef8eb615ea80c2a6244fd735e0f4ffed5fa93b9e
Giovanni Silva <git@gsilva.pro>
2024-08-21 20:23:54 -0300

    Exemplo de leitura de arquivos

--- src/main/java/br/org/fundatec/solidaula/model/openfile/OpenFileMain.java
+++ src/main/java/br/org/fundatec/solidaula/model/openfile/OpenFileMain.java
@@ -17,14 +17,34 @@ public class OpenFileMain {
         System.out.println("file content: ");
         int readByte = 0;
         try {
-
+            StringBuilder builder = new StringBuilder();
             while ((readByte = br.read()) != -1) {
-                System.out.print((char) readByte);
+                builder.append((char) readByte);
             }
+            System.out.println(builder.toString());
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+
+        try {
+            StringBuilder builder = new StringBuilder();
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            System.out.println("Lendo apenas as 2 primeiras linhas:");
+            int index = 0;
+            do {
+                line = br.readLine();
+                index ++;
+                builder.append(line + "\n");
+            } while (line != null && index < 2);
+            System.out.println(builder.toString());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        System.out.println("===== Fim ler duas linhas");
         // Usando new IO
         String arquivo = Files.readString(Path.of(FILE_PATH));
+        System.out.println(arquivo);
+        Files.readAllLines(Path.of(FILE_PATH)).forEach(System.out::println);
     }
 }
